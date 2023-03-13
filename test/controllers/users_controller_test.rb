c84e2661873b24904401d664445e1b3b09482877
aml <andrewmloop@gmail.com>
2023-03-13 17:36:45 -0400

    Add users controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,38 +1,56 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @user = users(:one)
-  # end
+  test 'should create user' do
+    assert_difference('User.count') do
+      post users_url, params:
+        { user:
+            { email: "#{Random.rand(999_999)}@test.com",
+              first_name: 'test',
+              password: 'Password1',
+              password_confirmation: 'Password1' } },
+                      as: :json
+    end
 
-  # test "should get index" do
-  #   get users_url, as: :json
-  #   assert_response :success
-  # end
+    assert_response :created
+  end
 
-  # test "should create user" do
-  #   assert_difference("User.count") do
-  #     post users_url, params: { user: { email: @user.email, first_name: @user.first_name, password: @user.password } }, as: :json
-  #   end
+  test 'should get index' do
+    get users_url, as: :json
+    assert_response :success
+  end
 
-  #   assert_response :created
-  # end
+  test 'should show user' do
+    @user = User.find_by({ email: 'test@test.com' })
 
-  # test "should show user" do
-  #   get user_url(@user), as: :json
-  #   assert_response :success
-  # end
+    get user_url(@user), as: :json
+    assert_response :success
+  end
 
-  # test "should update user" do
-  #   patch user_url(@user), params: { user: { email: @user.email, first_name: @user.first_name, password: @user.password } }, as: :json
-  #   assert_response :success
-  # end
+  test 'should update user' do
+    @user = User.find_by({ email: 'test@test.com' })
 
-  # test "should destroy user" do
-  #   assert_difference("User.count", -1) do
-  #     delete user_url(@user), as: :json
-  #   end
+    patch user_url(@user),
+          params: { user: {
+            first_name: @user.first_name
+          } }, as: :json
+    assert_response :success
+  end
 
-  #   assert_response :no_content
-  # end
+  test 'should destroy user' do
+    random_email = "#{Random.rand(999_999)}@test.com"
+
+    @user = User.create({
+                          first_name: 'test',
+                          email: random_email,
+                          password: 'Password1',
+                          password_confirmation: 'Password1'
+                        })
+
+    assert_difference('User.count', -1) do
+      delete user_url(@user), as: :json
+    end
+
+    assert_response :no_content
+  end
 end
