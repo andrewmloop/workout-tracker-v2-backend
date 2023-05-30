6f5962870755eebc72b1efc3d7778905197d5c85
aml <andrewmloop@gmail.com>
2023-05-30 16:09:37 -0400

    Update exercise DTOs

---
+++ src/exercise/dto/create-exercise.dto.ts
@@ -0,0 +1,43 @@
+import { IsIn, IsNotEmpty } from 'class-validator';
+import {
+  CATEGORY_ENUM,
+  LEVEL_ENUM,
+  MUSCLE_ENUM,
+} from '../../utils/enums/exercise.enum';
+
+export class CreateExerciseDto {
+  @IsNotEmpty({
+    message: 'A name is required',
+  })
+  name: string;
+
+  @IsNotEmpty({
+    message: 'A level is required',
+  })
+  @IsIn(LEVEL_ENUM, {
+    message: 'That is not a valid level',
+  })
+  level: string;
+
+  @IsNotEmpty({
+    message: 'A level is required',
+  })
+  @IsIn(MUSCLE_ENUM, {
+    message: 'That is not a valid muscle',
+  })
+  primaryMuscles: string[];
+
+  @IsIn(MUSCLE_ENUM, {
+    message: 'That is not a valid muscle',
+  })
+  secondaryMuscles: string[];
+
+  equipment: string;
+
+  @IsIn(CATEGORY_ENUM, {
+    message: 'That is not a valid category',
+  })
+  category: string;
+
+  instructions: string[];
+}
