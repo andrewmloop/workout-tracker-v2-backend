d6e70412e54750658129051cd5a002deefc4833e
aml <andrewmloop@gmail.com>
2023-06-03 10:40:29 -0400

    Add routine service and service tests

---
+++ src/routine/routine.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRoutineDto } from './dto/create-routine.dto';
+import { UpdateRoutineDto } from './dto/update-routine.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Routine } from './schemas/routine.schema';
+import { Model, Types } from 'mongoose';
+
+@Injectable()
+export class RoutineService {
+  constructor(
+    @InjectModel(Routine.name) private routineModel: Model<Routine>,
+  ) {}
+
+  async create(createRoutineDto: CreateRoutineDto): Promise<Routine> {
+    return await this.routineModel.create(createRoutineDto);
+  }
+
+  async findAll(): Promise<Routine[]> {
+    return await this.routineModel.find();
+  }
+
+  async findOne(id: Types.ObjectId): Promise<Routine> {
+    return await this.routineModel.findById(id);
+  }
+
+  async update(
+    id: Types.ObjectId,
+    updateRoutineDto: UpdateRoutineDto,
+  ): Promise<Routine> {
+    return await this.routineModel.findByIdAndUpdate(id, updateRoutineDto, {
+      new: true,
+    });
+  }
+
+  // TODO: Add functions to add/remove exercises from routine
+
+  // TODO: Add functions to set target sets on reps on exercise in routine
+
+  async remove(id: Types.ObjectId): Promise<boolean> {
+    return await this.routineModel.findByIdAndRemove(id);
+  }
+}
