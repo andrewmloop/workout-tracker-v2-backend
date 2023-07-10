3cc806f1e89088dfeb1423c5028cb4ad28eb662d
aml <andrewmloop@gmail.com>
2023-07-09 21:41:53 -0400

    Provide app server through ip address available on local network

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { networkInterfaces } from 'os';
 
 async function bootstrap() {
+  const net = networkInterfaces();
+  const ipAddress = net['en0'].find((x) => x.family === 'IPv4').address;
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
@@ -11,6 +14,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3000, ipAddress);
 }
 bootstrap();
